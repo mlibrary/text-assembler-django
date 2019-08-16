f2e8fb4981b6a215bc527c863dc021697598b4e5
Megan Schanz <schanzme@msu.edu>
2019-08-16 11:29:48 -0400

    Completed admin users page

--- textassembler_web/urls.py
+++ textassembler_web/urls.py
@@ -15,7 +15,9 @@ urlpatterns = [
     path('admin/users', views.admin_users, name='admin_users'),
     path('admin/searches', views.admin_searches, name='admin_searches'),
     path('admin/statistics', views.admin_statistics, name='admin_statistics'),
+    path('add/admin', views.add_admin_user, name='add'),
     url(r'^ajax/filter_val_input/(?P<filter_type>.+)$', views.get_filter_val_input, name='filter_val_input'),
     url(r'^delete/(?P<search_id>[0-9]+)/$', views.delete_search, name='delete'),
     url(r'^download/(?P<search_id>[0-9]+)/$', views.download_search, name='download'),
+    url(r'^delete/admin/(?P<userid>[A-Za-z0-9]+)/$', views.delete_admin_user, name='delete_admin_user'),
 ]
--- textassembler_web/views/admin_users.py
+++ textassembler_web/views/admin_users.py
@@ -6,7 +6,6 @@ from textassembler_web.models import administrative_users
 from textassembler_web.utilities import get_is_admin
 
 
-
 def admin_users(request):
     '''
     Render the users page
@@ -25,5 +24,59 @@ def admin_users(request):
 
     response["users"] = administrative_users.objects.all().order_by('userid')
 
-
     return render(request, 'textassembler_web/users.html', response)
+
+
+def add_admin_user(request):
+    '''
+    Add the given users as an administrator
+    '''
+    # Verify that the user is logged in and an admin
+    if not request.session.get('userid', False) or not get_is_admin(request.session['userid']):
+        return redirect('/login')
+
+    # Parse the userid to add
+    if "userid" in request.POST:
+        userid = request.POST["userid"]
+        if len(userid) > 1:
+            userid == userid[0]
+
+    # Validate the userid meets minimum requirements
+    if len(userid) == 0:
+        request.session["error_message"] = "the User ID must be at least 1 character long."
+
+    # Make sure the user is not already marked as an administrator
+    admin_record = administrative_users.objects.all().filter(userid=userid)
+    if admin_record:
+        request.session["error_message"] = "The User ID provided is already in the system as an administrator."
+
+    # Add the user to the administrator table
+    if request.session["error_message"] == "":
+        administrative_users.objects.create(userid=userid)
+
+    # Refresh the page
+    return redirect(admin_users)
+
+def delete_admin_user(request, userid):
+    '''
+    delete the given user from the administrators table
+    '''
+    # Verify that the user is logged in and an admin
+    if not request.session.get('userid', False) or not get_is_admin(request.session['userid']):
+        return redirect('/login')
+
+    # Validate that the user is not the current user
+    if userid == request.session['userid']:
+       request.session["error_message"] = "Can not remove the currently logged in user as an administrator." 
+
+    # Check if the user is in the administrators table
+    admin_record = administrative_users.objects.all().filter(userid=userid)
+    if not admin_record:
+         request.session["error_message"] = "The User ID provided was not found in the database! If this persists, please contact a system administrator."
+
+    # If they are, delete the record
+    if request.session["error_message"] == "":
+        admin_record.delete()
+
+    # Refresh the page
+    return redirect(admin_users)
