d97bd7ee9f154aa4fc9a606d7a4ce76f79265276
Megan Schanz <schanzme@msu.edu>
2019-08-16 13:41:52 -0400

    Got statistics page up

--- textassembler_web/views/admin_statistics.py
+++ textassembler_web/views/admin_statistics.py
@@ -1,10 +1,14 @@
 '''
 Handles all web requests for the statistics page
 '''
+import datetime
+from django.utils import timezone
+from django.utils.timezone import make_aware
 from django.shortcuts import render, redirect
 from textassembler_web.utilities import get_is_admin
-
-
+from textassembler_web.models import searches, historical_searches
+from django.db.models import Q
+from django.apps import apps
 
 def admin_statistics(request):
     '''
@@ -14,5 +18,39 @@ def admin_statistics(request):
     if not request.session.get('userid', False) or not get_is_admin(request.session['userid']):
         return redirect('/login')
 
+    # Default the date range to the current month
+    from_date = timezone.now().replace(day=1)
+    to_date = timezone.now()
 
-    return render(request, 'textassembler_web/statistics.html', {})
+    # Parse the provided range if available
+    if 'FromDate' in request.POST and 'ToDate' in request.POST:
+        from_date = make_aware(datetime.datetime.strptime(request.POST['FromDate'],'%Y-%m-%d'))
+        to_date = make_aware(datetime.datetime.strptime(request.POST['ToDate'],'%Y-%m-%d'))
+
+    # Remove time from datetimes
+    from_date = from_date.replace(hour=0,minute=0,second=0,microsecond=0)
+    to_date = to_date.replace(hour=0,minute=0,second=0,microsecond=0)
+
+    # Get the statistics from the database
+    search_recs = searches.objects.filter(Q(date_completed_compression__lte=to_date) & Q(date_completed_compression__gte=from_date))
+    search_hist_recs = searches.objects.filter(Q(date_completed_compression__lte=to_date) & Q(date_completed_compression__gte=from_date))
+    searches_complete = len(search_recs) + len(search_hist_recs)
+
+    num_results_downloaded = 0
+    for rec in search_recs:
+        num_results_downloaded = num_results_downloaded + (rec.num_results_downloaded if rec.num_results_downloaded is not None else 0)
+    for rec in search_hist_recs:
+        num_results_downloaded = num_results_downloaded + (rec.num_results_downloaded if rec.num_results_downloaded is not None else 0)
+
+    api_log = apps.get_model('textassembler_processor', 'api_log')
+    api_recs = api_log.objects.filter(Q(request_date__lte=to_date) & Q(request_date__gte=from_date) & Q(request_url__icontains="expand=PostFilters"))
+    site_searches_run = len(api_recs)
+
+    response = {
+        "searches_complete":searches_complete,
+        "files_downloaded":num_results_downloaded,
+        "site_searches_run":site_searches_run,
+        "from_date": from_date,
+        "to_date": to_date}
+
+    return render(request, 'textassembler_web/statistics.html', response)
