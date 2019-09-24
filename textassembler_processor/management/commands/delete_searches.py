4c70b4b2ce285cf79c0b21b0c466f886d46af50d
Megan Schanz <schanzme@msu.edu>
2019-09-24 16:20:29 -0400

    Refactored deletion processor into functions like the rest of the daemons

--- textassembler_processor/management/commands/delete_searches.py
+++ textassembler_processor/management/commands/delete_searches.py
@@ -24,11 +24,12 @@ class Command(BaseCommand):
     def __init__(self):
         self.terminate = False
         self.cur_search = None
-        self.retry = False
         self.searches = None
+        self.retry_counts = {"storage":0, "database":0, "filesystem":0}
+
         super().__init__()
 
-    def handle(self, *args, **options): # pylint: disable=too-many-branches, too-many-statements
+    def handle(self, *args, **options): # pylint: disable=too-many-branches
         '''
         Handles the command when run from the command line.
         '''
@@ -37,7 +38,6 @@ class Command(BaseCommand):
 
         self.terminate = False
         self.cur_search = None
-        self.retry = False
 
         # Grab the necessary models
         self.searches = apps.get_model('textassembler_web', 'searches')
@@ -47,108 +47,132 @@ class Command(BaseCommand):
             time.sleep(1) # take a quick break!
             try:
                 # check that there are items in the queue to be deleted based on date completed/failed
-                try:
-                    # delete searches completed/failed before this date
-                    delete_date = timezone.now() -  datetime.timedelta(days=settings.NUM_MONTHS_KEEP_SEARCHES * 30)
-                    queue = self.searches.objects.filter(
-                        Q(date_completed_compression__lte=delete_date)|Q(failed_date__lte=delete_date)|Q(deleted=True)).order_by('-update_date')
-                    if queue:
-                        self.cur_search = queue[0]
-                        self.retry = False
-                    else:
-                        self.retry = False
-                        continue # nothing to process
-                except Exception as ex: # pylint: disable=broad-except
-                    if not self.retry:
-                        logging.warning(f"Deletion Processor failed to retrieve the deletion queue. Will try again in {settings.DB_WAIT_TIME} seconds. {ex}")
-                        time.sleep(settings.DB_WAIT_TIME) # wait and re-try (giving this more time in case db server is being rebooted)
-                        self.retry = True
-                        continue
-                    else:
-                        log_error(f"Stopping. Deletion Processor failed to retrieve the pending deletion queue. {ex}")
-                        self.terminate = True
-                        continue
+                (queue, cont) = self.get_queue()
+                if cont or not queue or self.terminate:
+                    continue
+
 
                 # verify the storage location is accessibly
-                if not os.access(settings.STORAGE_LOCATION, os.W_OK) or not os.path.isdir(settings.STORAGE_LOCATION):
-                    log_error(f"Deletion Processor failed due to storage location being inaccessible or not writable. {settings.STORAGE_LOCATION}")
-                    # wait and retry, if the storage is still not available, then terminate
-                    time.sleep(settings.STORAGE_WAIT_TIME)
-                    if not os.access(settings.STORAGE_LOCATION, os.W_OK) or not os.path.isdir(settings.STORAGE_LOCATION):
-                        log_error(f"Stopping. Deletion Processor failed due to {settings.STORAGE_LOCATION} being inaccessible or not writable.")
-                        self.terminate = True
-                        continue
+                cont = self.check_storage()
+                if cont or self.terminate:
+                    continue
 
                 # check that there are items in the queue to be deleted based on date completed/failed
                 # we need to recheck this in case it changed while waiting for the storage
                 # location to become accessible
-                try:
-                    # delete searches completed/failed before this date
-                    delete_date = timezone.now() -  datetime.timedelta(days=settings.NUM_MONTHS_KEEP_SEARCHES * 30)
-                    queue = self.searches.objects.filter(
-                        Q(date_completed_compression__lte=delete_date)|Q(failed_date__lte=delete_date)|Q(deleted=True)).order_by('-update_date')
-                    if queue:
-                        self.cur_search = queue[0]
-                        self.retry = False
-                    else:
-                        self.retry = False
-                        continue # nothing to process
-                except Exception as ex: # pylint: disable=broad-except
-                    logging.error(ex)
-                    if not self.retry:
-                        logging.warning(f"Deletion Processor failed to retrieve the deletion queue. Will try again in {settings.DB_WAIT_TIME} seconds. {ex}")
-                        time.sleep(settings.DB_WAIT_TIME) # wait and re-try (giving this more time in case db server is being rebooted)
-                        self.retry = True
-                        continue
-                    else:
-                        log_error(f"Stopping. Deletion Processor failed to retrieve the pending deletion queue. {ex}")
-                        self.terminate = True
-                        continue
+                (queue, cont) = self.get_queue()
+                if cont or not queue or self.terminate:
+                    continue
+                self.cur_search = queue[0]
 
-                #  remove the files`
-                logging.info(f"Started removal of files for search {self.cur_search.search_id}")
-                save_location = os.path.join(settings.STORAGE_LOCATION, str(self.cur_search.search_id))
-                zip_path = find_zip_file(self.cur_search.search_id)
-
-                if os.path.isdir(save_location):
-                    try:
-                        shutil.rmtree(save_location)
-                    except OSError as ex1:
-                        log_error(f"Could not delete files for search {self.cur_search.search_id}. {ex1}", self.cur_search)
-                if zip_path != None and os.path.exists(zip_path):
-                    try:
-                        os.remove(zip_path)
-                    except OSError as ex2:
-                        log_error(f"Could not delete the zipped file for search {self.cur_search.search_id}. {ex2}", self.cur_search)
-                if os.path.isdir(save_location):
-                    try:
-                        os.rmdir(save_location)
-                    except OSError as ex3:
-                        log_error(f"Could not delete root directory for search {self.cur_search.search_id}. {ex3}", self.cur_search)
-                logging.info(f"Completed deletion of files for search {self.cur_search.search_id}")
+                #  remove the files
+                self.delete_search_files()
 
                 # delete the search record
-                try:
-                    self.cur_search.delete()
-                except OperationalError as oexp:
-                    time.sleep(settings.DB_WAIT_TIME) # wait and re-try (giving this more time in case db server is being rebooted)
-                    try:
-                        self.cur_search.delete()
-                    except OperationalError as oexp:
-                        log_error("Stopping. A database error occured while trying to delete the record to the database", oexp)
-                        self.terminate = True
-                        continue
-
+                cont = self.delete_search_record()
+                if cont or not queue or self.terminate:
+                    continue
 
             except Exception as exp: #pylint: disable=broad-except
                 # This scenario shouldn't happen, but handling it just in case
                 # so that the service won't quit on-error
-                log_error(f"An unexpected error occurred while deleting old searches. {create_error_message(exp, os.path.basename(__file__))}")
+                log_error((f"An unexpected error occurred while deleting old searches. "
+                           f"{create_error_message(exp, os.path.basename(__file__))}"))
                 self.terminate = True # stop the service since something is horribly wrong
                 continue
 
         # any cleanup after terminate
         logging.info("Stopped compression processing.")
+
+    def get_queue(self):
+        '''
+        Get the searches to be deleted
+        Returns:
+            queue (list): Items to be deleted
+            cont (bool): If the loop should continue or not
+        '''
+        try:
+            # delete searches completed/failed before this date
+            delete_date = timezone.now() -  datetime.timedelta(days=settings.NUM_MONTHS_KEEP_SEARCHES * 30)
+            queue = self.searches.objects.filter(
+                Q(date_completed_compression__lte=delete_date)|Q(failed_date__lte=delete_date)|Q(deleted=True)).order_by('-update_date')
+            self.retry_counts["database"] = 0
+            if queue:
+                return (queue, False)
+        except OperationalError as ex:
+            if self.retry_counts["database"] <= settings.NUM_PROCESSOR_RETRIES:
+                logging.warning(f"Deletion Processor failed to retrieve the deletion queue. Will try again in {settings.DB_WAIT_TIME} seconds. {ex}")
+                time.sleep(settings.DB_WAIT_TIME) # wait and re-try (giving this more time in case db server is being rebooted)
+                self.retry_counts["database"] = self.retry_counts["database"] + 1
+            else:
+                log_error(f"Stopping. Deletion Processor failed to retrieve the pending deletion queue. {ex}")
+                self.terminate = True
+        return (None, True)
+
+    def check_storage(self):
+        '''
+        Check to see if the storage location is available
+        Returns:
+            continue (bool): If you need to continue the loop
+        '''
+        if not os.access(settings.STORAGE_LOCATION, os.W_OK) or not os.path.isdir(settings.STORAGE_LOCATION):
+            if self.retry_counts["storage"] <= settings.NUM_PROCESSOR_RETRIES:
+                logging.error(f"Deletion Processor failed due to storage location being inaccessible or not writable. {settings.STORAGE_LOCATION}")
+                # wait and retry, if the storage is still not available, then terminate
+                time.sleep(settings.STORAGE_WAIT_TIME)
+                self.retry_counts["storage"] = self.retry_counts["storage"] + 1
+            else:
+                log_error(f"Stopping. Deletion Processor failed due to storage location being inaccessible or not writable. {settings.STORAGE_LOCATION}")
+                self.terminate = True
+            return True
+        self.retry_counts["storage"] = 0
+        return False
+
+    def delete_search_files(self):
+        '''
+        Delete the files on the storage location for the current search
+        '''
+        logging.info(f"Started removal of files for search {self.cur_search.search_id}")
+        save_location = os.path.join(settings.STORAGE_LOCATION, str(self.cur_search.search_id))
+        zip_path = find_zip_file(self.cur_search.search_id)
+
+        if os.path.isdir(save_location):
+            try:
+                shutil.rmtree(save_location)
+            except OSError as ex1:
+                log_error(f"Could not delete files for search {self.cur_search.search_id}. {ex1}", self.cur_search)
+        if zip_path != None and os.path.exists(zip_path):
+            try:
+                os.remove(zip_path)
+            except OSError as ex2:
+                log_error(f"Could not delete the zipped file for search {self.cur_search.search_id}. {ex2}", self.cur_search)
+        if os.path.isdir(save_location):
+            try:
+                os.rmdir(save_location)
+            except OSError as ex3:
+                log_error(f"Could not delete root directory for search {self.cur_search.search_id}. {ex3}", self.cur_search)
+        logging.info(f"Completed deletion of files for search {self.cur_search.search_id}")
+
+    def delete_search_record(self):
+        '''
+        Delete the search record  from the database
+        Returns:
+            cont (bool): If the loop should continue
+        '''
+        try:
+            self.cur_search.delete()
+            self.retry_counts["database"] = 0
+            return False
+        except OperationalError as ex:
+            if self.retry_counts["database"] <= settings.NUM_PROCESSOR_RETRIES:
+                time.sleep(settings.DB_WAIT_TIME) # wait and re-try (giving this more time in case db server is being rebooted)
+                self.retry_counts["database"] = self.retry_counts["database"] + 1
+            else:
+                log_error((f"Stopping. Deletion Processor failed due to a database connectivity issue.",
+                           f"(search id={'N/A' if self.cur_search is None else self.cur_search.search_id}.",
+                           f" {create_error_message(ex, os.path.basename(__file__))}"))
+                self.terminate = True
+            return True
 
     def sig_term(self, _, __):
         '''
