4ecb02b150ba4e3fa15eba6b5841bbdc0125183e
Megan Schanz <schanzme@msu.edu>
2019-12-06 10:30:42 -0500

    Convert to timezone before returning

--- textassembler_web/ln_api.py
+++ textassembler_web/ln_api.py
@@ -282,7 +282,7 @@ def is_in_run_window():
             f"End time: {end_time.strftime('%A %I:%M%p')}")
         logging.debug(message)
         return (False, start_time)
-    return (True, make_aware(start_time))
+    return (True, timezone.make_aware(start_time))
 
 def update_limits(service, headers):
     '''
