43366ecf763e703dd2fc9c8b88c6f4e301c1e27f
Megan Schanz <schanzme@msu.edu>
2019-12-09 09:04:17 -0500

    Added check to see if datetime was already timezone aware

--- textassembler_web/ln_api.py
+++ textassembler_web/ln_api.py
@@ -94,7 +94,8 @@ class LNAPI:
             if limits.remaining_per_minute == 0 and limits.reset_on_minute < timezone.now():
                 return timezone.now()
         # Else calculate time remaining to wait
-        all_dates = [limits.reset_on_day, limits.reset_on_hour, limits.reset_on_minute, timezone.make_aware(start_time)]
+        start_time = timezone.make_aware(start_time) if not timezone.is_aware(start_time) else start_time
+        all_dates = [limits.reset_on_day, limits.reset_on_hour, limits.reset_on_minute, start_time]
         if limit_type == 'download':
             return max(all_dates)
         if limits.remaining_per_day == 0:
